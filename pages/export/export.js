a5036109b4d9b8052cb56ecdd3968592a06f6bb8
huxiaoping <huxiaoping@dodoca.com>
2017-05-03 16:21:12 +0800

    修改样式

--- pages/export/export.js
+++ pages/export/export.js
@@ -6,38 +6,62 @@ Page({
       loading:false,
       disabled:false,
       modal:false,
-      modal1:false
+      modal1:false,
+      switch1:true,
+      switch2:true,
+      switch3:false,
+      switch4:false,
+      switch5:false,
+      checkbox1:false,
+      checkbox2:false
   },
   //事件处理函数
-  switchChange: function (e){
+  switchChange1: function (e){
     console.log('switch1 发生 change 事件，携带值为', e.detail.value)
   },
-  checkbox1Change: function(e) {
+  switchChange2: function (e){
+    console.log('switch2 发生 change 事件，携带值为', e.detail.value)
+  },
+  switchChange3: function (e){
+    console.log('switch3 发生 change 事件，携带值为', e.detail.value)
+  },
+  switchChange4: function (e){
+    console.log('switch4 发生 change 事件，携带值为', e.detail.value)
+  },
+  switchChange5: function (e){
+    console.log('switch5 发生 change 事件，携带值为', e.detail.value)
+  },
+  checkboxChange: function(e) {
     console.log('checkbox发生change事件，携带value值为：', e.detail.value)
   },
-   checkbox2Change: function(e) {
-    console.log('checkbox发生change事件，携带value值为：', e.detail.value)
+  checkAll:function(e){
+     var that = this;
+    var value = e.detail.value;
+    if(value.indexOf("1") != -1){
+        that.setData({
+           checkbox1:true,
+           checkbox2:true
+       })
+    }else{
+      that.setData({
+           checkbox1:false,
+           checkbox2:false
+       })
+    }
+    
   },
-   checkbox3Change: function(e) {
-    console.log('checkbox发生change事件，携带value值为：', e.detail.value)
-  },
-   checkbox4Change: function(e) {
-    console.log('checkbox发生change事件，携带value值为：', e.detail.value)
-  },
-   checkbox5Change: function(e) {
-    console.log('checkbox发生change事件，携带value值为：', e.detail.value)
-  },
-   checkbox6Change: function(e) {
-    console.log('checkbox发生change事件，携带value值为：', e.detail.value)
-  },
-   checkbox7Change: function(e) {
-    console.log('checkbox发生change事件，携带value值为：', e.detail.value)
-  },
-  openModal1:function(){
+   openModal1:function(){
       var that = this;
        that.setData({
            modal:true,
            modal1:true
+       })
+  },
+  closeModal:function(){
+     var that = this;
+       that.setData({
+           modal:false,
+           modal1:false
        })
   },
   sure1:function(){
