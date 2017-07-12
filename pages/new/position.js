47fbcb272a42953f0f05a49985a00f5c3a0070de
huxiaoping <huxiaoping@dodoca.com>
2017-07-12 13:49:20 +0800

    修改

--- pages/new/position.js
+++ pages/new/position.js
@@ -5,14 +5,22 @@ Page({
   data: {
     tab_active:1,
     style:"",
-     offices:['请选择科室','外科','内科','中医科','中西医结合科','妇产科','儿科','医技科'],
+    offices:['请选择科室','外科','内科','中医科','中西医结合科','妇产科','儿科','医技科'],
     officeIndex:0,
     bespeak_array:[[false,false,false,false,false,false,false],[false,false,false,false,false,false,false],[false,false,false,false,false,false,false]],
     choice_one:[false,false,false,false,false,false,false],
     choice_two:[false,false,false],
     choice_three:[false,false,false,false,false,false,false,false,false],
     loading:false,
-    disabled:false
+    disabled:false,
+     offices2:['请选择科室','外科','内科','中医科','中西医结合科','妇产科','儿科','医技科'],
+    officeIndex2:0,
+    choice_one2:[false,false,false,false,false,false,false],
+    choice_two2:[false,false,false],
+    choice_three2:[false,false,false,false,false,false,false,false,false],
+    loading2:false,
+    disabled2:false
+   
   },
  //选择tab
   tabClick:function(e){
@@ -52,6 +60,12 @@ Page({
             officeIndex: e.detail.value
       })
   },
+  //全职
+   bindOfficeChange2:function(e){
+     this.setData({
+            officeIndex2: e.detail.value
+      })
+  },
   //医院等级
   choiceCheck1:function(e){
       var that = this;
@@ -70,8 +84,56 @@ Page({
             choice_one:choice_one
       });
   },
+   //医院等级（全职）
+  choiceSelect1:function(e){
+      var that = this;
+      var currentTarget = e.currentTarget;
+      var index = 0;
+      if(currentTarget){
+        index = currentTarget.dataset.index;
+      }
+       var choice_one2 = that.data.choice_one;
+      if(choice_one2[index]){
+          choice_one2[index]= false;
+      }else{
+         choice_one2[index] = true;
+      } 
+      that.setData({
+            choice_one2:choice_one2
+      });
+  },
   //公司性质
   choiceCheck2:function(e){
+      var that = this;
+      var currentTarget = e.currentTarget;
+      var index = 0;
+      if(currentTarget){
+        index = currentTarget.dataset.index;
+      }
+      var choice_two = that.data.choice_two;
+      choice_two = [false,false,false];
+       choice_two[index] = true;
+      that.setData({
+            choice_two:choice_two
+      });
+  },
+   //公司性质（全职）
+  choiceSelect2:function(e){
+      var that = this;
+      var currentTarget = e.currentTarget;
+      var index = 0;
+      if(currentTarget){
+        index = currentTarget.dataset.index;
+      }
+      var choice_two2 = that.data.choice_two2;
+      choice_two2 = [false,false,false];
+       choice_two2[index] = true;
+      that.setData({
+            choice_two2:choice_two2
+      });
+  },
+  //公司类型
+  choiceCheck3:function(e){
       var that = this;
       var currentTarget = e.currentTarget;
       var index = 0;
@@ -88,14 +150,23 @@ Page({
             choice_three:choice_three
       });
   },
-  //公司类型
-  choiceCheck3:function(e){
+   //公司类型（全职）
+  choiceSelect3:function(e){
       var that = this;
       var currentTarget = e.currentTarget;
       var index = 0;
       if(currentTarget){
         index = currentTarget.dataset.index;
       }
+      var choice_three2 = that.data.choice_three2;
+       if(choice_three2[index]){
+          choice_three2[index]= false;
+      }else{
+         choice_three2[index] = true;
+      } 
+      that.setData({
+            choice_three2:choice_three2
+      });
   },
   //允许
   allowChange:function (e){
@@ -103,6 +174,10 @@ Page({
   },
   //保存
   save:function(){
+
+  },
+  //保存
+  save2:function(){
 
   },
   onLoad: function (option) {
